e1aff1d31e7a3f26c0106c6845f3c330acac1d8f
weare0 <lay3699@naver.com>
2024-11-14 05:06:59 +0900

    [D1] Title: 더블더블, Time: 133 ms, Memory: 20,668 KB -BaekjoonHub

---
+++ SWEA/D1/2019. 더블더블/더블더블.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        for (int i = 0; i <= n; i++) {
+            System.out.print((int) Math.pow(2, i) + " ");
+        }
+        sc.close();
+    }
+}
